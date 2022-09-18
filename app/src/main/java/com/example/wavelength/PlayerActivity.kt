06db9d94001ac83846143e6cd54e57a526409ec5
Ruikang (Leo) Guo <leokwok1997@hotmail.com>
2022-09-18 17:00:06 -0400

    users can now favorite/unfavorite a song

--- app/src/main/java/com/example/wavelength/PlayerActivity.kt
+++ app/src/main/java/com/example/wavelength/PlayerActivity.kt
@@ -2,6 +2,11 @@ package com.example.wavelength
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
@@ -9,12 +14,22 @@ import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import coil.load
-import coil.transform.CircleCropTransformation
+import com.example.wavelength.api.MusicAPI
 import com.example.wavelength.databinding.ActivityPlayerBinding
 import com.example.wavelength.model.Song
+import com.example.wavelength.retrofit.RetrofitInstance
+import com.example.wavelength.service.MusicAPIService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
 import java.io.IOException
+import java.lang.Exception
 
 
 private const val SONG_KEY = "song"
@@ -42,22 +57,29 @@ class PlayerActivity : AppCompatActivity() {
         val tvSongArtist = findViewById<TextView>(R.id.tvSongArtist)
         val ivPlay = findViewById<ImageView>(R.id.ivPlay)
         val ivAlbumArt = findViewById<ImageView>(R.id.ivAlbumArt)
+        val ivFav = findViewById<ImageView>(R.id.ivFav)
 
-        var playing = false
+        supportActionBar?.apply {
+            title = "Now playing"
+            elevation = 0F
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
 
         player = MediaPlayer()
 
+        // launch activity with intent
         val launchIntent = intent
         val song: Song? = launchIntent.extras?.getParcelable<Song>(SONG_KEY)
 
-        // test player
+        // init
         var streamURL = ""
         var albumURL = ""
         if (song != null) {
             // init album art
             albumURL = "https://musiclibrary.nyc3.cdn.digitaloceanspaces.com/${song.songName}.jpeg"
 
-            // test player
+            // init player
             streamURL = "https://musiclibrary.nyc3.cdn.digitaloceanspaces.com/${song.songName}.mp3"
             tvSongTitle.text = song.songName
             tvSongAlbum.text = song.albumName
@@ -79,37 +101,55 @@ class PlayerActivity : AppCompatActivity() {
         }
 
         ivPlay.setOnClickListener {
-            if (!playing) {
+            if (!player.isPlaying) {
+                Toast.makeText(this, "Now playing: ${song?.songName} by ${song?.artistName}", Toast.LENGTH_SHORT).show()
                 ivPlay.setImageResource(R.drawable.ic_pause)
                 player.start()
-                playing = true
             } else {
                 ivPlay.setImageResource(R.drawable.ic_play)
                 player.pause()
-                playing = false
             }
             Log.i("status", player.isPlaying.toString())
-            Toast.makeText(this, "Now playing: ${song?.songName} by ${song?.artistName}", Toast.LENGTH_SHORT).show()
         }
 
-
         // set album art
-//        https://musiclibrary.nyc3.cdn.digitaloceanspaces.com/Too%20Good%20At%20Goodbyes.jpeg
-//        ivAlbumArt.setImageResource (albumURL)
         ivAlbumArt.load(albumURL) {
             crossfade(true)
         }
 
-        // add back button
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // render fav button
+        if (song?.isFavorite == true) {
+            ivFav.setImageResource(R.drawable.ic_heart)
+            ivFav.setColorFilter(ContextCompat.getColor(this, R.color.orange))
+        }
 
-        // change actionbar title
-        supportActionBar?.title = "Now playing"
+        suspend fun favBtnCallback(song: Song) = coroutineScope {
+            async { RetrofitInstance.api.favSong(song.id, !song.isFavorite) }
+        }.await()
 
+        ivFav.setOnClickListener{
+            try {
+                runBlocking {
+                    if (song != null) {
+                        favBtnCallback(song)
+                        if (song.isFavorite) {
+                            ivFav.setImageResource(R.drawable.ic_heart_outline)
+                            ivFav.setColorFilter(ContextCompat.getColor(this@PlayerActivity, R.color.dark_gray))
+                        } else {
+                            ivFav.setImageResource(R.drawable.ic_heart)
+                            ivFav.setColorFilter(ContextCompat.getColor(this@PlayerActivity, R.color.orange))
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                Log.i("post-error", e.message.toString())
+            }
+        }
     }
 
     // add back button
     override fun onSupportNavigateUp(): Boolean {
+        player.stop()
         onBackPressed()
         return true
     }
--- app/src/main/java/com/example/wavelength/api/MusicAPI.kt
+++ app/src/main/java/com/example/wavelength/api/MusicAPI.kt
@@ -3,10 +3,14 @@ package com.example.wavelength.api
 import com.example.wavelength.model.Song
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface MusicAPI {
     // for testing
     @GET("all")
     suspend fun getSongs(): Response<List<Song>>
 
+    @POST("fav/{id}/{fav}")
+    suspend fun favSong(@Path("id") id: String, @Path("fav") fav: Boolean)
 }
