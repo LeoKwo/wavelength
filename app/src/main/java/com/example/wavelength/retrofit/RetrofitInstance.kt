d43d366c04f0f6e542c6a7f260e2e693887dc0f0
Ruikang (Leo) Guo <leokwok1997@hotmail.com>
2022-11-08 01:58:31 -0500

    api now map to deployed backend

--- app/src/main/java/com/example/wavelength/retrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/wavelength/retrofit/RetrofitInstance.kt
@@ -7,8 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api: MusicAPI by lazy {
         Retrofit.Builder()
-//            .baseUrl("https://wavelength-app.herokuapp.com/api/")// heroku deploy (for deployment)
-            .baseUrl("http://10.0.2.2:8093/api/") // 10.0.2.2 is localhost (for localhost testing)
+            .baseUrl("https://wavelength-app.herokuapp.com/api/")// heroku deploy (for deployment)
+//            .baseUrl("http://10.0.2.2:8093/api/") // 10.0.2.2 is localhost (for localhost testing)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MusicAPI::class.java)
